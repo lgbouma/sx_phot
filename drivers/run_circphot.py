7965cd87897253da86ad38ebed7ac14f627b541d
lgbouma <bouma.luke@gmail.com>
2026-01-14 11:36:13 -0800

    circphot: get_supplemented_sx_spectrum

--- drivers/run_circphot.py
+++ drivers/run_circphot.py
@@ -1,31 +1,13 @@
 
-## A star calib
-#get_sx_spectrum(
-#    ra_deg=0.8568686344700,
-#    dec_deg=-46.830975607410
-#)
+from sx_phot.circphot import (
+    get_sx_spectrum, get_supplemented_sx_spectrum
+)
 
-## TOI-837
-#get_sx_spectrum(
-#    ra_deg=157.03727470138,
-#    dec_deg=-64.50520903903,
-#    star_id='TOI-837',
-#    output_dir='test_results'
-#)
-
-from sx_phot.circphot import get_sx_spectrum
-
-#get_sx_spectrum(
-#        ra_deg=64.11714603424,
-#        dec_deg=28.12648069800,
-#        star_id='LkCa_4',
-#        output_dir='test_results',
-#        use_cutout=True
-#)
-
-get_sx_spectrum(
-        ra_deg=114.36670927895,
-        dec_deg=-66.75737858669,
+get_supplemented_sx_spectrum(
+        #ra_deg=114.36670927895,
+        #dec_deg=-66.75737858669,
+        ra_deg=114.36668585978607,
+        dec_deg=-66.75730245550294,
         star_id='TIC_300651846',
         output_dir='test_results',
         use_cutout=True
@@ -46,3 +28,19 @@ get_sx_spectrum(
 #        output_dir='test_results',
 #        use_cutout=True
 #)
+
+## A star calib
+#get_sx_spectrum(
+#    ra_deg=0.8568686344700,
+#    dec_deg=-46.830975607410
+#)
+
+## TOI-837
+#get_sx_spectrum(
+#    ra_deg=157.03727470138,
+#    dec_deg=-64.50520903903,
+#    star_id='TOI-837',
+#    output_dir='test_results'
+#)
+
+
