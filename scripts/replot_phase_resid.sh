394621e2a2e73c870b3e7ecf37691365058cd69b
lgbouma <bouma.luke@gmail.com>
2026-01-19 14:53:41 -0800

    stage scripts/replot_phase_resid.sh for history

---
+++ scripts/replot_phase_resid.sh
@@ -0,0 +1,2 @@
+python plot_phase_residuals.py --csv test_results/sxphot_cache_TIC_340638558_ra116p64144889593244_-58p835970928514094_annulus_splsupp.csv --t0 1505.395600 --period_days 1.441945 --ylim -0.25,0.18
+ python plot_phase_residuals.py --csv test_results/sxphot_cache_TIC_278865060_ra100p86574011431765_-57p20278687716049_annulus_splsupp.csv --t0 1594.379000 --period_days 1.254462 --ylim -0.3,0.2
